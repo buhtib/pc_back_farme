ee944ac628a7091c09f86fed4f8faa3a968f5d69
buhtib <2418531916@qq.com>
2020-10-13 15:41:45 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,17 @@ import commonRoutes from "./commonRoutes";
 
 // 初始化路由的时候，只挂载不需要用户权限的路由表
 const createRouter = () =>
-   new Router({
-      // mode: 'history', // require service support
-      scrollBehavior: () => ({
-         y: 0
-      }),
-      routes: commonRoutes
-   })
+    new Router({
+        // mode: 'history', // require service support
+        scrollBehavior: () => ({
+            y: 0
+        }),
+        routes: commonRoutes
+    });
 const router = createRouter();
-// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-   const newRouter = createRouter();
-   router.matcher = newRouter.matcher; // 重置 router
+    const newRouter = createRouter();
+    router.matcher = newRouter.matcher; // 重置 router
 }
 
 export default router;
