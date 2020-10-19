d0f45336758c7aae327f955e340973835dfea6c4
buhtib <2418531916@qq.com>
2020-10-19 09:33:08 +0800

    update 上传图片路径

--- src/api/url.js
+++ src/api/url.js
@@ -1,5 +1,7 @@
 //存放所有的公共 url
 
 //上传图片
-export const uploadImgUrl = 'http://192.168.1.170:8080/minio/upload'
-
+export const uploadImgUrl =
+    process.env.NODE_ENV == "development"
+        ? "http://192.168.1.170:8080/minio/upload"
+        : "/minio/upload";
