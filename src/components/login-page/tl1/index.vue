ef3af7da2a3e851e70da44c4376d74bb0a4e7ae9
buhtib <2418531916@qq.com>
2020-10-19 10:13:39 +0800

    update 解决动画卡顿问题

--- src/components/login-page/tl1/index.vue
+++ src/components/login-page/tl1/index.vue
@@ -3,16 +3,16 @@
         <div class="effect-c">
             <span
                 v-for="(item,index) in 40"
-                :style="{left: (item * 2.5) + '%',
-                animationDuration:  2 + (Math.random() * 6) + 's',
+                :style="{
+                    left: (item * 2.5) + '%',
                 animationDelay: 1 + (Math.random() * 8) + 's'}"
 				:class="`animate__animated animate__fadeOutUp animate__infinite animate__tl1 animate__tl1-${randomS(1, 4)}x`"
 			></span>
 			<span
-				v-for="(item,index) in 14"
+				v-for="(item,index) in 20"
 				:style="{left: randomS(1, 101) + '%',
-                animationDuration:  2 + (Math.random() * 8) + 's',
-                animationDelay: 2 + (Math.random() * 6) + 's'}"
+                top: randomS(1, 101) + '%',
+                animationDelay: 1 + (Math.random() * 6) + 's'}"
 				:class="`animate__animated animate__fadeOutUp animate__infinite animate__tl1 animate__tl1-${randomS(1, 4)}x`"
 			></span>
 		</div>
@@ -52,23 +52,29 @@ export default {
             display: inline-block;
             position: absolute;
             left: 1px;
-            bottom: -100%;
+            bottom: 0;
             height: 100%;
             width: 100%;
         }
 
         .animate__tl1-1x {
-            background: url(../../../assets/img/login/point_1.png) no-repeat;
-            background-size: 14px 14px;
-        }
-        .animate__tl1-2x {
-            background: url(../../../assets/img/login/point_1.png) no-repeat;
-            background-size: 12px 12px;
-        }
-        .animate__tl1-3x {
-            background: url(../../../assets/img/login/point_1.png) no-repeat;
-            background-size: 10px 10px;
-        }
+			width: 14px;
+			height: 14px;
+			background: url(../../../assets/img/login/point_1.png) no-repeat;
+			background-size: 100% 100%;
+		}
+		.animate__tl1-2x {
+			width: 12px;
+			height: 12px;
+			background: url(../../../assets/img/login/point_1.png) no-repeat;
+			background-size: 100% 100%;
+		}
+		.animate__tl1-3x {
+			width: 10px;
+			height: 10px;
+			background: url(../../../assets/img/login/point_1.png) no-repeat;
+			background-size: 100% 100%;
+		}
     }
 }
 </style>
